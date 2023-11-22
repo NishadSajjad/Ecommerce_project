3058595cef1c439f17b3336f6aec597b12206133
Sajjad Nishad <nishad@Sajjads-MacBook-Air.local>
2023-11-23 00:50:27 +0600

    Initial commit

---
+++ resources/views/admin/pendingOrder.blade.php
@@ -0,0 +1,8 @@
+@extends('admin.layouts.template')
+@section('page_title')
+  PendingOrder
+@endsection
+@section('content')
+hello from order
+
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,70 @@
+<?php
+
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\OrderController;
+use App\Http\Controllers\Admin\ProductController;
+use App\Http\Controllers\Admin\SubCategoryController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware(['auth'])
+->group(function () {
+    Route::controller(DashboardController::class)->group(function(){
+        Route::get('/admin/dashboard', 'index')->name('admindashboard');
+    });
+    
+    Route::controller(CategoryController::class)->group(function(){
+        Route::get('/admin/all-category', 'index')->name('allcategory');
+        Route::get('/admin/add-category', 'addCategory')->name('addcategory');
+
+    });
+
+    Route::controller(SubCategoryController::class)->group(function(){
+        Route::get('/admin/all-Subcategory', 'index')->name('allSubcategory');
+        Route::get('/admin/add-Subcategory', 'addSubCategory')->name('addSubcategory');
+
+    });
+   
+    Route::controller(ProductController::class)->group(function(){
+        Route::get('/admin/all-Product', 'index')->name('allProduct');
+        Route::get('/admin/add-Product', 'addProduct')->name('addProduct');
+
+    });
+   
+    Route::controller(OrderController::class)->group(function(){
+        Route::get('/admin/pending-Order', 'index')->name('pendingOrder');
+        
+    });
+   
+
+});
+
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
